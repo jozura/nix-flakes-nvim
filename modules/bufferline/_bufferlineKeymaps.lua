c2b7b9da414f226ec517df17585e1a3a1dc94428
Johannes Haapakoski <johannes.haapakoski@outlook.com>
2023-05-21 14:59:07 +0300

    Remap buffer handling keys

--- modules/bufferline/_bufferlineKeymaps.lua
+++ modules/bufferline/_bufferlineKeymaps.lua
@@ -1,7 +1,9 @@
 function keymaps()
     -- Cycle between buffers
-    vim.keymap.set("n", "<C-H>", ":bp<CR>", {})
-    vim.keymap.set("n", "<C-L>", ":bn<CR>", {})
+    vim.keymap.set("n", "<C-Left>", ":bp<CR>", {})
+    vim.keymap.set("n", "<C-Right>", ":bn<CR>", {})
+    -- Switch to next buffer and close the previous one
+    vim.keymap.set("n", "<C-q>", ":bn<CR>:bd #<CR>")
 end
 
 return keymaps
