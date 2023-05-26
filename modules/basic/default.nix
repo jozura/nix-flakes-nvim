bc73d451e9647f48072be574b41f87a1567801a9
Johannes Haapakoski <johannes.haapakoski@outlook.com>
2023-05-26 21:42:51 +0300

    Add some plugins for clj

--- modules/basic/default.nix
+++ modules/basic/default.nix
@@ -1,10 +1,19 @@
 {pkgs, ...}: {
   startPackages = with pkgs.vimPlugins; [
+    # Show indention lines
     indent-blankline-nvim
-    vim-sleuth
+    # Automatically adjust indents
+    # vim-sleuth
+    # Autocomplete parens etc.
     auto-pairs
+    # Show git changes next to the number col
     gitsigns-nvim
+    # Find files, git actions, etc.
     telescope-nvim
+    # S-expression editing
+    vim-sexp
+    # Rainbow parens
+    rainbow
   ];
   optPackages = [];
   additionalDependencies = [pkgs.ripgrep];
