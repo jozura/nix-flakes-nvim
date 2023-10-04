2972be62a329be9ad6e4647467ff1ee8f29dd5f7
Johannes Haapakoski <johannes.haapakoski@outlook.com>
2023-10-04 21:26:20 +0300

    Start to reorganize packages and plugins

--- flake.nix
+++ flake.nix
@@ -4,38 +4,36 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    vim-illuminate = {
-      url = "github:RRethy/vim-illuminate/master";
-      flake = false;
-    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
-    vim-illuminate,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        startPkgs = map 
-          ({src, name}: pkgs.vimUtils.buildVimPlugin { inherit src; inherit name; })
-          [
-            {name = "vim-illuminate"; src = vim-illuminate;}
-          ];
-        optPkgs = map 
-          ({src, name}: pkgs.vimUtils.buildVimPlugin { inherit src; inherit name; })
-          [
-          ];
+        startPkgs = with pkgs.vimPlugins; [
+          vim-illuminate 
+          gitsigns-nvim 
+          nvim-autopairs
+          telescope-nvim
+          vim-sexp
+        ];
+        optPkgs = with pkgs.vimPlugins; [
+          vim-sexp
+        ];
+        additionalDeps = with pkgs; [
+          ripgrep
+        ];
 
         pkgs = nixpkgs.legacyPackages.${system};
         moduleConfig = (import ./modules) pkgs;
         myNeovim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
           configure = {
             packages.myVimPackage = {
-              start = moduleConfig.start ++ [startPkgs] ++ [optPkgs];
-              opt = moduleConfig.opt;
+              start = moduleConfig.start ++ startPkgs;
+              opt = moduleConfig.opt ++ optPkgs;
             };
             customRC = moduleConfig.customRC;
           };
@@ -44,7 +42,7 @@
       in {
         packages.default = pkgs.writeShellApplication {
           name = "myvim";
-          runtimeInputs = [myNeovim] ++ moduleConfig.additionalDependencies;
+          runtimeInputs = [myNeovim] ++ moduleConfig.additionalDependencies ++ additionalDeps;
           text = ''
             export ENABLED_MODULES="${enabledModules}"
             export LUA_PATH="${moduleConfig.luaPath};"
--- modules/basic/default.nix
+++ modules/basic/default.nix
@@ -1,19 +1,7 @@
 {pkgs, ...}: {
   startPackages = with pkgs.vimPlugins; [
-    # Automatically adjust indents
-    # vim-sleuth
-    # Autocomplete parens etc.
-    auto-pairs
-    # Show git changes next to the number col
-    gitsigns-nvim
-    # Find files, git actions, etc.
-    telescope-nvim
-    # S-expression editing
-    vim-sexp
-    # Rainbow parens
-    rainbow
   ];
   optPackages = [];
-  additionalDependencies = [pkgs.ripgrep];
+  additionalDependencies = [];
 }
 
