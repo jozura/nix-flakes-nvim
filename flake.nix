3149e4993b751ddc3074452f70033bbf99725f03
Johannes Haapakoski <johannes.haapakoski@outlook.com>
2023-12-03 04:40:33 +0200

    Docker package

--- flake.nix
+++ flake.nix
@@ -72,11 +72,45 @@
         additionalDeps = with pkgs; [
           # Required by telescope plugin for fuzzy finding
           ripgrep
+          git
+          # Required by nix language server (nil)
+          nix
           # Language servers (that have no flakes)
           lua-language-server
         	nodePackages.typescript-language-server
         ];
         luaConfig = (import ./lua) pkgs;
+        alpineImage = pkgs.dockerTools.pullImage {
+          imageName = "alpine";
+          imageDigest = "sha256:34871e7290500828b39e22294660bee86d966bc0017544e848dd9a255cdf59e0";
+          sha256 = "0z8vhhr9sqb9mpnczdjqhhg2rka7zv2k4vcm7kymb3il50bqhqdg";
+        };
+        nonRootShadowSetup = { user, uid, gid ? uid }: with pkgs; [
+          (
+          writeTextDir "etc/shadow" ''
+            root:!x:::::::
+            ${user}:!:::::::
+          ''
+          )
+          (
+          writeTextDir "etc/passwd" ''
+            root:x:0:0::/root:${runtimeShell}
+            ${user}:x:${toString uid}:${toString gid}::/home/${user}:
+          ''
+          )
+          (
+          writeTextDir "etc/group" ''
+            root:x:0:
+            ${user}:x:${toString gid}:
+          ''
+          )
+          (
+          writeTextDir "etc/gshadow" ''
+            root:x::
+            ${user}:x::
+          ''
+          )
+        ];
         myNeovim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
           configure = {
             packages.myVimPackage = {
@@ -86,7 +120,7 @@
             customRC = luaConfig.customRC;
           };
         };
-      in {
+      in rec {
         packages.default = pkgs.writeShellApplication {
           name = "myvim";
           runtimeInputs = [
@@ -98,6 +132,50 @@
           text = ''
             nvim "$@"
           '';
+        };
+        
+        # TODO: Refactor docker stuff into own file
+        packages.dockerImage = pkgs.dockerTools.streamLayeredImage {
+          fromImage = alpineImage;
+          name = "myvim";
+          tag = "1.0";
+          created = "now";
+          # uid/gid has to be same as host users
+          contents = nonRootShadowSetup {uid = 1000; user = "jozura";};
+          fakeRootCommands = ''
+            mkdir -p home/jozura
+            chown 1000 ./home/jozura
+            mkdir -p home/jozura/workdir
+          '';
+          config = {
+            Entrypoint = ["${packages.default}/bin/myvim"];
+            WorkingDir = "/workdir";
+            User = "jozura";
+            # Fix colors when running inside tmux
+            Env = ["TERM=screen"];
+          };
+        };
+
+        packages.withoutNetwork = pkgs.writeShellApplication {
+            name = "dvim";
+            runtimeInputs = [ pkgs.docker ];
+            text = ''
+              if ! docker images | grep -q "^myvim\s*1.0\s"; then
+                ${packages.dockerImage} | docker load
+              fi
+              if [ "$#" -lt 1 ]; then
+                docker run -it --rm --network none -v "$(pwd)":/workdir myvim:1.0
+              else
+                absolute_path=$(realpath "$1")
+                if [ -d "$absolute_path" ]; then
+                  docker run -it --rm --network none -v "$absolute_path":/workdir myvim:1.0
+                else
+                  dirname=$(dirname "$absolute_path")
+                  filename=$(basename "$absolute_path")
+                  docker run -it --rm --network none -v "$dirname":/workdir myvim:1.0 "$filename"
+                fi
+              fi
+            '';
         };
       }
     );
