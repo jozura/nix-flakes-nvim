bba7be5c5b52ca03851c7385a5cc759a2b1e5c5b
Johannes Haapakoski <johannes.haapakoski@outlook.com>
2025-10-09 01:26:15 +0300

    Fix other typo

--- lua/cfgs/whichkey_cfg.lua
+++ lua/cfgs/whichkey_cfg.lua
@@ -19,7 +19,7 @@ function M.setup()
     { "<leader>ga", "<cmd>Gitsign stage_hunk<cr>", desc = "Stage hunk" },
     { "<leader>gb", "<cmd>Gitsigns blame_line<cr>", desc = "Git Blame" },
     { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Git commits" },
-    { "<leader>g", group = "hunks" },
+    { "<leader>h", group = "hunks" },
     { "<leader>hn", "<cmd>Gitsigns next_hunk<cr>", desc = "Next hunk" },
     { "<leader>hp", "<cmd>Gitsigns prev_hunk<cr>", desc = "Prev hunk" },
     { "<leader>hr", "<cmd>Gitsigns reset_hunk<cr>", desc = "Prev hunk" },
