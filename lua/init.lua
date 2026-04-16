542afedb038580cd67cefaa9221eeddcbe074c72
Johannes Haapakoski <johannes.haapakoski@outlook.com>
2026-04-16 03:16:13 +0300

    Conjure

--- lua/init.lua
+++ lua/init.lua
@@ -11,6 +11,22 @@ local nvimAutopair = require("nvim_autopair_cfg")
 local marks = require("marks_cfg")
 local telescope = require("telescope_cfg")
 
+-- Conjure setup; disables LSP diagnostics from the Conjure output buffer =>
+-- no ugly red markings
+vim.api.nvim_create_autocmd('DiagnosticChanged', {
+  pattern = {"conjure-log-*"},
+  callback = function(args)
+    local diagnostics = args.data.diagnostics
+
+    if (diagnostics[1] ~= nil) then
+      local bufnr = diagnostics[1]["bufnr"]
+      local namespace = diagnostics[1]["namespace"]
+      vim.diagnostic.disable(bufnr)
+      vim.diagnostic.reset(namespace, bufnr)
+    end
+  end,
+})
+
 basic.setup()
 nvimTree.setup()
 gitsigns.setup()
